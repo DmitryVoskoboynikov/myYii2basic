dccce0b3827fd122b50919fc2bbf3c0cb7500b71
DmitryVoskoboynikov <dmitry.voskoboynikoff@gmail.com>
2025-06-23 16:27:42 +0300

    define constans: YII_DEBUG, YII_ENV

--- web/index.php
+++ web/index.php
@@ -3,3 +3,7 @@
 //phpinfo();
 //echo "Hello Nagval Warriors!!! I'm in yii2basic version 2.0.52";
 
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
